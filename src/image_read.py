36231a19c214c83a7db192b64f962761903e7b7f
alb <albgarjim1@gmail.com>
2020-08-31 23:15:46 +0200

    docs: updated usage

--- src/image_read.py
+++ src/image_read.py
@@ -3,5 +3,5 @@ import numpy as np
 from libs_image_filter import *
 
 
-# pixelated = filter_pixel("../tools/images/tiger.jpg")
+# filter_pixelated = filter_pixel("../tools/images/tiger.jpg")
 filter_ascii("../tools/images/elephant.jpg", 1200, 1200)
